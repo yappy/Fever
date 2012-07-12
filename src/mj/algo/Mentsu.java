87139aae6b156769ab5ed905fdf0085e826f7d20
yappy <superunyo@yahoo.co.jp>
2012-07-12 15:12:55 +0900

    pon, chi.

--- src/mj/algo/Mentsu.java
+++ src/mj/algo/Mentsu.java
@@ -14,21 +14,32 @@ public class Mentsu {
 	public boolean naki;
 	public int nakiAttr;
 	public boolean kan;
+	public boolean isKakan;
 
-	private Mentsu(Type type, int hai, boolean naki, int nakiAttr, boolean kan) {
+	private Mentsu(Type type, int hai, boolean naki, int nakiAttr, boolean kan,
+			boolean isKakan) {
 		this.type = type;
 		this.hai = hai;
 		this.naki = naki;
 		this.nakiAttr = nakiAttr;
 		this.kan = kan;
+		this.isKakan = isKakan;
 	}
 
 	public static Mentsu create(Type type, int hai) {
-		return new Mentsu(type, hai, false, 0, false);
+		return new Mentsu(type, hai, false, 0, false, false);
+	}
+
+	public static Mentsu chi(int start, int index) {
+		return new Mentsu(Type.SHUNTSU, start, true, index, false, false);
+	}
+
+	public static Mentsu pon(int hai, int from) {
+		return new Mentsu(Type.KOTSU, hai, true, from, false, false);
 	}
 
 	public Mentsu copy() {
-		return new Mentsu(type, hai, naki, nakiAttr, kan);
+		return new Mentsu(type, hai, naki, nakiAttr, kan, isKakan);
 	}
 
 	@Override
